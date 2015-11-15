4dcef38850c2867bfd0c60e549a7b0ac59b6fe4a
sajidsan <sajid@jut.io>
2015-11-14 18:36:41 -0800

    about page corrections from amy, committing before switching back to html from php

--- public_html/about.php
+++ public_html/about.php
@@ -13,23 +13,19 @@
           <img class="img-responsive" src="img/meThread_blue_lines.jpg">
         </div>
         <div class="col-md-8">
-          <h1>About me? I'm blushing.</h1>  
-          <p>I’m a product designer at <a href="http://jut.io">Jut</a> working on big data problems with an incredible team. I spend my after hours teaching a brilliant group of students at the <a href="http://www.aaunewmedia.com/learn-2/">Academy of Arts</a> and trying to find people that’ll work on fun side projects with me. Before coming to the Bay area, I studied design research and interaction design at the <a href="http://www.id.iit.edu">IIT Institute of Design</a> in Chicago and worked with the <a href="http://www.studio4designresearch.com">Studio for Design Research</a> in Hong Kong. When not dorking out on design, you can find me noodling around in a sketchbook and playing bad hip hop covers on my Casio keyboard.
+          <h1>About me? I'm blushing.</h1>
+          <p>I’m a product designer at <a href="http://jut.io">Jut</a> working on big data problems with an incredible team. I spend my after hours teaching a brilliant group of students at the <a href="http://www.aaunewmedia.com/learn-2/">Academy of Arts</a> and trying to find people that’ll work on fun side projects with me. Before coming to the Bay area, I studied design research and interaction design at the <a href="http://www.id.iit.edu">IIT Institute of Design</a> in Chicago and worked with the <a href="http://www.studio4designresearch.com">Studio for Design Research</a> in Hong Kong. I've also taught a General Assembly Class on Research + Strategy.
+          <a href="https://generalassemb.ly/online/videos/four-design-research-strategies-for-your-next-project">Check it out here</a>. When not dorking out on design, you can find me noodling around in a sketchbook and playing bad hip hop covers on my Casio keyboard.
             <br>
             <br>
-            I recently taught a General Assembly Class on Research + Strategy.
+            I just made <a href="https://www.google.com/maps/d/edit?mid=zNKevDtDBijc.ky0o1S7O3P5Y&usp=sharing">a list of restaurants in SF with drawing paper on their tables</a>.
             <br>
-            <a href="https://generalassemb.ly/online/videos/four-design-research-strategies-for-your-next-project">Check it out here</a>.
-            <br>
-            <br>
-            I just made <a href="http://www.aaunewmedia.com/learn-2/">a list of restaurants in SF with drawing paper on their tables</a>. 
-            <br>
-            Its a great place to start if you want to entice me to hang out, sketch dorky diagrams, and talk shop :) 
+            Want to talk? Pick out a place and we'll sketch dorky diagrams, and talk shop :)
             </p>
         </div>
       </div> <!-- end row -->
       <div class="row">
-      <h1>I'm trying to get better at getting back to people. Drop me an <a href="mailto:sajidresh@gmail.com">email</a> and see if its working.</h1> 
+      <h1>I'm trying to get better at getting back to people. Drop me an <a href="mailto:sajidresh@gmail.com">email</a> and see if its working.</h1>
     </div>
 
     <!--closing javascript -->
